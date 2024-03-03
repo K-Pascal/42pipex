379570cc53db1b155cc285e7cf4224579bab1b54
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-03-03 16:54:10 +0100

    Renamed function according to the last commit

--- src/parent_process.c
+++ src/parent_process.c
@@ -1,114 +1,101 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parent_process.c                                   :+:      :+:    :+:   */
+/*   parentt_process.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/09 14:44:22 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/01/09 15:09:00 by pnguyen-         ###   ########.fr       */
+/*   Created: 2024/01/09 14:58:56 by pnguyen-          #+#    #+#             */
+/*   Updated: 2024/01/09 16:26:11 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-#include "gnl/get_next_line.h"
 #include "libft/libft.h"
 
 #include "utils.h"
 
-static void	do_heredoc(t_data *data, int pipefd[2]);
-static void	read_input(t_data *data, int pipefd[2]);
-static char	*ask_input(void);
+static void	exec_inter_cmds(t_data *data, int pipefd[2]);
+static void	create_other_process(t_data *data, int i, int pipefd[2], int fd_in);
 
 void	parent_process(t_data *data, int pipefd[2])
 {
+	int		flags;
 	char	**argv;
 
-	if (close(pipefd[0]) == -1)
-		perror("parent_process():close(pipefd[0])");
-	redirect_pipefd(data, pipefd[1], STDOUT_FILENO);
-	if (data->limiter != NULL)
-		do_heredoc(data, pipefd);
-	else
-	{
-		pipefd[0] = open(data->f_in, O_RDONLY);
-		if (pipefd[0] == -1)
-		{
-			free_pipex(data, NULL, NULL, "parent_process():open()");
-			exit(EXIT_FAILURE);
-		}
-	}
+	if (close(pipefd[1]) == -1)
+		perror("child_process():close()");
+	exec_inter_cmds(data, pipefd);
 	redirect_pipefd(data, pipefd[0], STDIN_FILENO);
-	argv = ft_split(data->cmds[0], ' ');
+	flags = O_CREAT | O_WRONLY;
+	if (data->limiter != NULL)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	pipefd[1] = open(data->f_out, flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (pipefd[1] == -1)
+	{
+		free_pipex(data, NULL, NULL, "child_process():open()");
+		exit(EXIT_FAILURE);
+	}
+	redirect_pipefd(data, pipefd[1], STDOUT_FILENO);
+	argv = ft_split(data->cmds[data->nbr_cmds - 1], ' ');
 	exec_prog(argv[0], argv, data->envp);
 	my_free_all(argv);
 }
 
-static void	do_heredoc(t_data *data, int pipefd[2])
+static void	exec_inter_cmds(t_data *data, int pipefd[2])
+{
+	int	fds[2];
+	int	i;
+
+	i = 1;
+	while (i + 1 < data->nbr_cmds)
+	{
+		if (pipe(fds) == -1)
+		{
+			free_pipex(data, NULL, NULL, "exec_inter_cmds():pipe()");
+			close(pipefd[0]);
+			exit(EXIT_FAILURE);
+		}
+		create_other_process(data, i, fds, pipefd[0]);
+		wait(NULL);
+		if (close(fds[1]) == -1)
+			perror("close()");
+		if (close(pipefd[0]) == -1)
+			perror("close()");
+		pipefd[0] = fds[0];
+		i++;
+	}
+}
+
+static void	create_other_process(t_data *data, int i, int pipefd[2], int fd_in)
 {
 	pid_t	fpid;
+	char	**argv;
 
-	if (pipe(pipefd) == -1)
-	{
-		free_pipex(data, NULL, NULL, "do_heredoc():pipe()");
-		exit(EXIT_FAILURE);
-	}
 	fpid = fork();
 	if (fpid == -1)
 	{
-		free_pipex(data, NULL, pipefd, "do_heredoc():fork()");
+		free_pipex(data, NULL, pipefd, "create_other_process():fork()");
+		close(fd_in);
 		exit(EXIT_FAILURE);
 	}
 	if (fpid == 0)
 	{
-		read_input(data, pipefd);
-		free_pipex(data, NULL, NULL, NULL);
-		exit(EXIT_SUCCESS);
-	}
-	wait(NULL);
-	if (close(pipefd[1]) == -1)
-		perror("do_heredoc():close()");
-}
-
-static void	read_input(t_data *data, int pipefd[2])
-{
-	size_t	len_limiter;
-	char	*input;
-	char	*limiter;
-
-	if (close(pipefd[0]) == -1)
-		perror("read_input():close(pipefd[0])");
-	limiter = ft_strjoin(data->limiter, "\n");
-	if (!limiter)
-	{
-		free_pipex(data, NULL, NULL, "read_input():ft_strjoin()");
-		close(pipefd[1]);
+		if (close(pipefd[0]) == -1)
+			perror("create_other_process():close()");
+		redirect_pipefd(data, fd_in, STDIN_FILENO);
+		redirect_pipefd(data, pipefd[1], STDOUT_FILENO);
+		argv = ft_split(data->cmds[i], ' ');
+		exec_prog(argv[0], argv, data->envp);
+		my_free_all(argv);
+		my_n_free_all(data->cmds, data->nbr_cmds);
 		exit(EXIT_FAILURE);
 	}
-	len_limiter = ft_strlen(limiter);
-	input = ask_input();
-	while (input && !!ft_strncmp(input, limiter, len_limiter))
-	{
-		ft_putstr_fd(input, pipefd[1]);
-		free(input);
-		input = ask_input();
-	}
-	if (close(pipefd[1]) == -1)
-		perror("read_input():close(pipefd[1])");
-	free(input);
-	free(limiter);
-}
-
-static char	*ask_input(void)
-{
-	char	*input;
-
-	ft_putstr_fd("> ", STDIN_FILENO);
-	input = get_next_line(STDIN_FILENO);
-	return (input);
 }

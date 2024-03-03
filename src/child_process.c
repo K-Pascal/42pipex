f60fb61cbcc4b1735e9d70ae0ad7f9a14121af5d
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-03-03 16:54:11 +0100

    Auxiliary functions are now working

--- src/child_process.c
+++ src/child_process.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 14:44:22 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/01/12 16:26:02 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/01/15 19:12:16 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,46 +21,34 @@
 
 #include "utils.h"
 
-static int	do_heredoc(t_data *data, int fds[2]);
+static int	do_heredoc(t_data *data);
 static void	read_input(t_data *data, int pipefd[2]);
 static char	*ask_input(void);
 
-//void	child_process(t_data *data, int pipefd[2])
-//{
-//	if (close(pipefd[0]) == -1)
-//		perror("child_process():close(pipefd[0])");
-//	redirect_pipefd(pipefd[1], STDOUT_FILENO);
-//	choose_input(data, pipefd);
-//	redirect_pipefd(pipefd[0], STDIN_FILENO);
-//	prepare_command(data, 0);
-//}
-
-int	choose_input(t_data *data, int fds[2])
+int	choose_input(t_data *data)
 {
 	int	fd_in;
 
 	if (data->limiter != NULL)
-		fd_in = do_heredoc(data, fds);
+		fd_in = do_heredoc(data);
 	else
 	{
 		if (access(data->f_in, F_OK | R_OK) == -1)
 		{
 			perror(data->f_in);
-			close_pipe(fds);
 			exit(EXIT_FAILURE);
 		}
 		fd_in = open(data->f_in, O_RDONLY);
 		if (fd_in == -1)
 		{
 			perror("child_process():open()");
-			close_pipe(fds);
 			exit(EXIT_FAILURE);
 		}
 	}
 	return (fd_in);
 }
 
-static int	do_heredoc(t_data *data, int fds[2])
+static int	do_heredoc(t_data *data)
 {
 	pid_t	fpid;
 	int		pipefd[2];
@@ -68,7 +56,6 @@ static int	do_heredoc(t_data *data, int fds[2])
 	if (pipe(pipefd) == -1)
 	{
 		perror("do_heredoc():pipe()");
-		close_pipe(fds);
 		exit(EXIT_FAILURE);
 	}
 	fpid = fork();
@@ -76,16 +63,15 @@ static int	do_heredoc(t_data *data, int fds[2])
 	{
 		perror("do_heredoc():fork()");
 		close_pipe(pipefd);
-		close_pipe(fds);
 		exit(EXIT_FAILURE);
 	}
 	if (fpid == 0)
 	{
-		close_pipe(fds);
 		read_input(data, pipefd);
 		exit(EXIT_SUCCESS);
 	}
-	waitpid(0, NULL, WUNTRACED);
+	if (waitpid(fpid, NULL, WUNTRACED) == -1)
+		perror("do_heredoc():waitpid()");
 	if (close(pipefd[1]) == -1)
 		perror("do_heredoc():close()");
 	return (pipefd[0]);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:13:44 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/01/12 17:55:56 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/01/15 20:51:59 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@
 #define MSG2 "Usage : ./pipex here_doc [LIMITER] [cmd 1] [cmd 2] [output file]\n"
 
 static int	init_data(t_data *data, int argc, char **argv, char **envp);
-static void	split_process(t_data *data, int *status);
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -40,8 +39,8 @@ int	main(int argc, char **argv, char **envp)
 	}
 	start = init_data(&data, argc, argv, envp);
 	data.cmds = &argv[start];
-	parent_process(&data, &status);
-	split_process(&data, &status);
+	status = 0;
+	pipex(&data, &status);
 	if (WIFEXITED(status))
 		status = WEXITSTATUS(status);
 	else
@@ -76,24 +75,4 @@ static int	init_data(t_data *data, int argc, char **argv, char **envp)
 	data->cmds = NULL;
 	data->envp = envp;
 	return (start);
-}
-
-static void	split_process(t_data *data, int *status)
-{
-	return ;
-	pid_t	fpid;
-
-	fpid = fork();
-	if (fpid == -1)
-	{
-		perror("split_process():fork()");
-		exit(EXIT_FAILURE);
-	}
-	if (fpid == 0)
-		parent_process(data, status);
-	else
-	{
-		while (waitpid(-1, status, WUNTRACED) > 0)
-			;
-	}
 }

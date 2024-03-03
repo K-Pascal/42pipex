2af6efc38d59a99695a736739d9999b62624b574
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-03-03 16:54:11 +0100

    ft_strjoin() fixed

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:39:17 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/12/03 18:30:40 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/01/17 15:16:43 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ char	*ft_strjoin(char const s1[], char const s2[])
 	if (dest == 0)
 		return (0);
 	ft_strlcpy(dest, s1, len1 + 1);
-	ft_strlcpy(dest + len1, s2, len1 + len2 + 1);
+	ft_strlcpy(dest + len1, s2, len2 + 1);
 	return (dest);
 }

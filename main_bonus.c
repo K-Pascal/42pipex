e55d89e90cba71b9c79556bbbae977c65802daa6
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-03-09 18:08:20 +0100

    remove 2 cmds limits for here_doc

--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:13:44 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/03 18:17:28 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/09 17:53:26 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@
 
 #include "process.h"
 
-#define MSG1 "Usage: ./pipex_bonus [input file] [cmd 1] ... [cmd n] [output file]\n"
-#define MSG2 "Usage: ./pipex_bonus here_doc [LIMITER] [cmd 1] [cmd 2] [output file]\n"
+#define MSG1 "Usage: ./pipex_bonus [input file] [cmd 1] ... \
+[cmd n] [output file]\n"
+
+#define MSG2 "Usage: ./pipex_bonus here_doc [LIMITER] [cmd 1] ... \
+[cmd n] [output file]\n"
+
 #define SIG_RETURN 128
 
 static void	init_data(t_data *data, int argc, char **argv, char **envp);
@@ -51,7 +55,7 @@ static void	init_data(t_data *data, int argc, char **argv, char **envp)
 {
 	if (ft_strncmp(argv[1], "here_doc", 9) == 0)
 	{
-		if (argc != 6)
+		if (argc < 6)
 		{
 			ft_putendl_fd(MSG2, STDERR_FILENO);
 			exit(EXIT_FAILURE);
